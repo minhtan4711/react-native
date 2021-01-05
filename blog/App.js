1139808143a1674f5f19fee8d1d4422e1e372cdc
minhtan <minhtan471100@gmail.com>
2021-01-06 00:10:48 +0700

    IndexScreen and add context

--- blog/App.js
+++ blog/App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="IndexScreen">
+        <Stack.Screen
+          name="IndexScreen"
+          component={IndexScreen}
+          options={{ title: "Index Screen" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
