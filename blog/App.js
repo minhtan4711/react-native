62e366315211b16f6eaac178bf39408093fde82b
minhtan <minhtan471100@gmail.com>
2021-01-07 14:50:15 +0700

    refactor

--- blog/App.js
+++ blog/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import IndexScreen from "./src/screens/IndexScreen";
-import { BlogProvider } from "./src/context/BlogContext";
+import { Provider } from "./src/context/BlogContext";
 
 const Stack = createStackNavigator();
 
@@ -23,8 +23,8 @@ const App = () => {
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 };
