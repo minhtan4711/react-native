b3d9f41a0b7fd8a3f827b16e150713e440eb45e0
uongminhtan <minhtan471100@gmail.com>
2021-02-20 22:57:08 +0700

    add headerRight edit button

--- blog/src/screens/ShowScreen.js
+++ blog/src/screens/ShowScreen.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-const ShowScreen = ({ route }) => {
+const ShowScreen = ({ route, navigation }) => {
   console.log(route.params.id);
+  const blogPost = route.params;
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("EditScreen", {
+              id: blogPost.id,
+              title: blogPost.title,
+              content: blogPost.content,
+            })
+          }
+        >
+          <Feather name="edit-2" size={24} color="black" />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View>
-      <Text>{route.params.id}</Text>
+      <Text>{blogPost.id}</Text>
+      <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
 };
