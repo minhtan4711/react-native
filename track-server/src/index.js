e4d593e298420846e9e6156b3ff6bfcee7750910
uongminhtan <minhtan471100@gmail.com>
2021-03-02 23:56:18 +0700

    add some comments and add middlewares

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,14 +1,21 @@
+//root
 require('./models/User') //define one time
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') //automatically pass information associated with the body property of incoming request 
 const authRoutes = require('./routes/authRoutes')
+const requireAuth = require('../middlewares/requireAuth')
 
-const app = express()
+//use express api
+const app = express() //present all the application with some root handlers
 
+//root handler: send information signup through express API
 app.use(bodyParser.json())
+//import signin signup procedure: root handler
 app.use(authRoutes)
 
+
+//connect to mongodb database
 const mongoUri = 'mongodb+srv://minhtan471100:mynewbike85751@track-server.m9ugh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
     useNewUrlParser: true,
@@ -23,10 +30,10 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongoose', err)
 })
 
-app.get('/', (req, res) => {
-    res.send("Hi there")
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`)
 })
-
+//listen to port
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
