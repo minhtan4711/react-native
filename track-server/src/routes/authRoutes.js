bcab02714fdfd54370f1997ce397a01f084e9c67
uongminhtan <minhtan471100@gmail.com>
2021-03-02 23:57:57 +0700

    add comments and jwt and sigin logic

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,16 +1,29 @@
+//authentication request handling
 const express = require('express')
 const mongoose = require('mongoose')
+// token send back to user for future request
+const jwt = require('jsonwebtoken')
+//acess to user's model
 const User = mongoose.model('User') //check the email if trung or not
 
 const router = express.Router()
 
+
+//place all information to req object (req.body)
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body
+    // email user could be duplicate
+    try {
+        const user = new User({ email, password })
+        await user.save()
 
-    const user = new User({ email, password })
-    await user.save()
-
-    res.send('You made a post request')
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+        res.send({ token })
+    } catch (err) {
+        return res.status(422).send(err.message) // 422: Invalid data
+    }
 })
 
 module.exports = router
+
+//postman: tell the server that we got some information attach to post request => head to Headers tab and add fill Content-Type to Key prop and application/json to value prop  
